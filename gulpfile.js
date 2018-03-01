a742acecdd79010377130dc4fcc4cc559e57cc27
Felipe Mult <felipeteixeira@mult-e.com.br>
2018-03-01 01:21:27 -0300

    correcoes de paths do gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,46 +1,73 @@
-var gulp            = require("gulp");
-var browserSync     = require("browser-sync").create();
-var sass            = require("gulp-sass");
-var sourcemaps      = require('gulp-sourcemaps');
-var spritesmith 	= require('gulp.spritesmith');
-var gulpSequence    = require('gulp-sequence')
+var gulp          = require("gulp");
+    browserSync   = require("browser-sync").create(),
+    sass          = require("gulp-sass"),
+    sourcemaps    = require('gulp-sourcemaps'),
+    spritesmith 	= require('gulp.spritesmith'),
+    gulpSequence  = require('gulp-sequence'),
+    historyApi    = require('connect-history-api-fallback');
+
+
+// PATHS SRC
+var paths = {
+    html: {
+        input:  'app/**/*.html'
+    },
+    sass: {
+        input:  'app/assets/scss/**/*.scss',
+        output: 'app/assets/css',
+        sprite: 'app/assets/scss/module'
+    },
+    sprite: {
+        input:  'app/assets/img/sprite/*.*',
+        output: 'app/assets/img/',
+        imgName: 'sprite.png',
+        cssName: '_sprite.scss',
+        imgPath: '/assets/img/sprite.png' 
+    },
+    server: {
+        input:  ['app/**/*.html', 'app/**/*.js']
+    }   
+};
+
 
 gulp.task("serve", ["sass"], function() {
     browserSync.init({
-        server: "./app"
+        server: "app",
+        middleware: [ historyApi() ]
     });
 
-    gulp.watch("./app/assets/scss/**/*.scss", ["sass"]);
-    gulp.watch(['app/**/*.html', './app/**/*.js']).on("change", browserSync.reload);
+    gulp.watch(paths.sass.input, ["sass"]);
+    gulp.watch([paths.server.input]).on("change", browserSync.reload);
 });
 
 // CSS
 gulp.task("sass", function() {
-    return gulp.src("./app/assets/scss/**/*.scss")
+    return gulp.src(paths.sass.input)
         .pipe(sourcemaps.init())
         .pipe(sass({
             outputStyle: "compressed"
         }).on("error", sass.logError))
         .pipe(sourcemaps.write())
-        .pipe(gulp.dest("./app/assets/css"))
+        .pipe(gulp.dest(paths.sass.output))
         .pipe(browserSync.stream());
 });
 
 gulp.task('sprite', function () {
   
-  	var spriteData = gulp.src('./app/assets/img/sprite/*.*')
-  		.pipe(spritesmith({
-		imgName: 'sprite.png',
-		cssName: '_sprite.scss',
-    	imgPath: '/assets/img/sprite.png' 
+  	var spriteData = gulp.src(paths.sprite.input)
+
+		.pipe(spritesmith({
+		    imgName: paths.sprite.imgName,
+		    cssName: paths.sprite.cssName,
+        imgPath: paths.sprite.imgPath
   	}));
 
   	var imgStream = spriteData.img
-    .pipe(gulp.dest('./app/assets/img/'));
+    .pipe(gulp.dest(paths.sprite.output));
 
   	var cssStream = spriteData.css
     
-    .pipe(gulp.dest('./app/assets/scss/module'));
+    .pipe(gulp.dest(paths.sass.sprite));
 
   	return gulpSequence(imgStream, cssStream);
 });
